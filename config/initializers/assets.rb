06b91a04eafa12fcb1608fc8af685f090df1a0f0
Adrian Agius <agiuslaw@gmail.com>
2016-03-26 08:55:14 +1100

    quick push before clean disk

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,11 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( jquery.js )
 Rails.application.config.assets.precompile += %w( typed.js )
+Rails.application.config.assets.precompile += %w( application.js)
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( cbpAnimatedHeader.js )
+Rails.application.config.assets.precompile += %w( jquery.appear.js )
+Rails.application.config.assets.precompile += %w( mooz.themes.scripts.js )
+Rails.application.config.assets.precompile += %w( npm.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
+Rails.application.config.assets.precompile += %w( SmoothScroll.min.js )
