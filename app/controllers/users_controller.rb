863a60a947b5a950364e11989ee098444b440359
Adrian Agius <agiuslaw@gmail.com>
2016-05-03 14:05:13 +1000

    hackathon site close to complete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to root_path, notice: 'Thanks for signing up, we will be in touch!' }
+        # format.json { render :show, status: :created, location: @user }
+        format.js
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :email, :shirtsize, :designation, :thoughts)
+  end
+end
---
+++ db/migrate/20160428141418_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.string :shirtsize
+      t.string :designation
+      t.string :thoughts
+
+      t.timestamps null: false
+    end
+  end
+end
